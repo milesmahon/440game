59b0f79298cc1e3e77984474e66a74686a3c1087
miles <mjm382@bu.edu>
2017-04-27 14:47:17 -0400

    clean up

--- autoRunAtropos.py
+++ autoRunAtropos.py
@@ -9,16 +9,16 @@ one = 0
 two = 0
 
 for i in range(numTimes):
-    subprocess.check_output(["java AtroposGame 7 './bwebPlayerE4' > log.txt"], shell=True, stderr=subprocess.STDOUT)
+    subprocess.check_output(["java AtroposGame 7 './bwebPlayer' > log.txt"], shell=True, stderr=subprocess.STDOUT)
 
     #print(subprocess.check_output(["grep 'has won' log.txt"], shell=True))
     out = subprocess.check_output(["grep 'has won' log.txt"], shell=True)
-    if ("Script One" in str(out)):
+    if ("Script" in str(out)):
         one+= 1
-    elif ("Script Two" in str(out)):
-        two+=1
-    elif ("Script" in str(out)):
-   	    one+=1
+    # elif ("Script Two" in str(out)):
+    #     two+=1
+    # elif ("Script" in str(out)):
+ #   	    one+=1
     elif("Default" in str(out)):
    	    two+=1
     else:
@@ -26,6 +26,5 @@ for i in range(numTimes):
 
     print("Game " + str(i) + " Completed. One: " + str(one) + " Two: " + str(two))
 
-
+print("Board size 7 with bwebPlayer to a depth of 8 eval 5")
 print("One wins with prob: " + str(one/(one+two)))
-
